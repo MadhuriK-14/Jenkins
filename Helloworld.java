23e0d679a47c6d4ceaad47ba0e00e742c15cdf28
MadhuriK-14 <72243094+MadhuriK-14@users.noreply.github.com>
2022-05-14 15:01:57 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ class Helloworld
 {
    public static void main( String [] args)
    {
-      System.out.print("Hello Madhuri BE IT 19 ");
+      System.out.print("Hello Madhuri BE IT 19 from batch 2021-2022");
    }
    
 }
